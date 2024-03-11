07924cdfd7eede1d7ec08fe39b1d4ebece4e5065
DD1C7470R <gracevalley101@gmail.com>
2024-03-11 23:38:00 +0000

    Change get index implementation hot fix 7

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -130,14 +130,14 @@ class FilesController {
         if (!ObjectId.isValid(parentId)) {
           return res.status(200).json([]);
         }
-        const parent = await fileCollections.find({ parentId: new ObjectID(parentId) }).toArray();
+        const parent = await fileCollections.find({ _id: new ObjectID(parentId) }).toArray();
         if (!parent.length || parent[0].type !== 'folder') {
           return res.status(200).json([]);
         }
         query.parentId = new ObjectID(parentId);
       }
 
-      const results = await fileCollections.find(query)
+      const results = await fileCollections.find({ ...query, _id: query.parentId })
         .skip(page * MAX_PAGE_SIZE).limit(MAX_PAGE_SIZE).toArray();
 
       const modifyResult = results.map((file) => ({
